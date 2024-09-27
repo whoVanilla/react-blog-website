048a7f63c22190a74ed5ac751737c0996d0b3115
Vanilla M <bidisaaaa@gmail.com>
2024-09-27 15:18:46 +0530

    adding nav bar

--- react-js-blog/src/App.jsx
+++ react-js-blog/src/App.jsx
@@ -1,12 +1,13 @@
 
 import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
 
   return (
     <>
-    <div>Navbar</div>
+    <Navbar/>
     <Outlet/>
     </>
   )
---
+++ react-js-blog/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+    const navItems = [
+        {path: "/", link: "Home"},
+        {path: "/services", link: "Services"},
+        {path: "/about", link: "About"},
+        {path: "/blogs", link: "Blogs"},
+        {path: "/contact", link: "Contact"},
+    ]
+  return (
+    <div>Navbar Page</div>
+  )
+}
+
+export default Navbar
