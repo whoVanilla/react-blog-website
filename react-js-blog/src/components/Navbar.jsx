3d7aa7f9ddfcbada9d08034550b0eb0450984216
Vanilla M <bidisaaaa@gmail.com>
2024-09-30 14:45:03 +0530

    Added dynamic menu for small screens

--- react-js-blog/src/components/Navbar.jsx
+++ react-js-blog/src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
       <header className='bg-black text-white fixed top-0 left-0 right-0'>
         <nav className='px-4 py-4 max-w-7xl mx-auto flex justify-between items-center'>
           <a href="/" className='text-xl font-bold text-white'>Design<span className='text-[#85d8ff]'>DK</span></a>
-          <ul className='md: flex gap-12 text-lg'>
+          <ul className='md:flex gap-12 text-lg hidden'>
             {
               navItems.map(({path, link}) => <li className='text-white hover:text-[#85d8ff] transition-all duration-200 ease-in' key={path}>
                 <NavLink to={path}>{link}</NavLink>
@@ -43,6 +43,16 @@ const Navbar = () => {
             </button>
           </div>
         </nav>
+        {/* Menu items only for mobile */}
+        <div>
+        <ul className={`md:hidden gap-12 text-lg block space-y-4 px-4 py-6 mt-14 bg-white ${isMenuOpen ? "fixed top-0 left-0 w-full transition-all ease-out duration-150" : "hidden"}`}>
+            {
+              navItems.map(({path, link}) => <li className='text-black' key={path}>
+                <NavLink onClick={toggleMenu} to={path}>{link}</NavLink>
+              </li>)
+            }
+          </ul>
+        </div>
       </header>
   )
 }
